2f41df1a928fc5fab026304e15a9c57a263511b2
Dieter Vandroemme <dieter@sizingservers.be>
2014-09-25 17:15:10 +0200

    Reading energy for westmere and nehalem does not work, reading the msr made the agent crash. Should be tested for Nehalem, Sandy Bridge, Ivy Bridge and Haswell

--- vApus-hmt/src/be/sizingservers/vapus/hmt/agent/cpu/IntelCPU.java
+++ vApus-hmt/src/be/sizingservers/vapus/hmt/agent/cpu/IntelCPU.java
@@ -56,9 +56,9 @@ public class IntelCPU extends CPU {
 
         determineArchitecture();
 
-        try{        
-            initMSRs(); 
-        }catch(Exception ex){          
+        try {
+            initMSRs();
+        } catch (Exception ex) {
         }
 
         this.hyperThreadingEnabled = this.logicalCores > this.physicalCores;
@@ -182,7 +182,13 @@ public class IntelCPU extends CPU {
      * @throws Exception
      */
     private float getJoulesPerEnergyUnit() throws Exception {
-        long energyStatusUnits = super.readMSR(Registers.MSR_RAPL_POWER_UNIT, 13, 8).longValue();
+        long energyStatusUnits = 0L;
+
+        try {
+            energyStatusUnits = super.readMSR(Registers.MSR_RAPL_POWER_UNIT, 13, 8).longValue();
+        } catch (Exception ex) {
+            //Ignore
+        }
 
         return 1f / (float) (1 << energyStatusUnits); //is the same as (1/2)^energy_status_units
     }
